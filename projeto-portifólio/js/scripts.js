dee4cd8f07561f22f556d6bb96c5530a5b6d6802
Eli S. Resedá <50843398+Ereseda@users.noreply.github.com>
2024-04-09 21:21:47 -0300

    Atualização de arquivos
    atualização de arquivos com implementação de JavaScript.

--- projeto-portifólio/js/scripts.js
+++ projeto-portifólio/js/scripts.js
@@ -1,3 +1,10 @@
 function openNavy(){
-    
+    var x= document.getElementById("navigation");
+    if(x.className === "navigation"){
+        x.className += " menujs";
+        document.getElementById("threeline-icon").innerHTML = "&Cross;";
+    }else{
+        x.className = "navigation";
+        document.getElementById("threeline-icon").innerHTML = "&#9776;";
+    }
 }
